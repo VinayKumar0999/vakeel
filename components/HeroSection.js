486aaa6ca4c7cc4106f698fde62470f3241e8234
Vinay Kumar Mareedu <krishnachaitanya@techouts.com>
2025-01-30 14:35:30 +0530

    UI fixes added

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -1,25 +1,53 @@
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card';
 
-const HeroSection = () => {
+const data = [
+  {
+    img: 'https://3veta.com/wp-content/uploads/2020/12/6.-Providing-online-consultations-for-lawyers-2.png',
+    title: "",
+    description: "",
+  }, {
+    img: 'https://www.vkeel.com/blog/wp-content/uploads/2020/02/Legal-Advice-Advocate-In-Delhi.jpg'
+  },
+  {
+    img: 'https://www.gulfadvocates.com/wp-content/uploads/2020/05/Knowing-When-to-Seek-Legal-Advice.jpg'
+  }, {
+    img: 'https://www.1law.com/wp-content/uploads/2022/02/vector-illustration-of-remote-consultation-with-lawyer-scaled.jpg'
+  }, {
+    img: 'https://media.licdn.com/dms/image/D5612AQEDrfZYx44AJg/article-cover_image-shrink_720_1280/0/1690771649024?e=2147483647&v=beta&t=RKYTOtB81FhLjRQcITlGfrWB9eAGUSS2Qx5SE574jf4'
+  }
+]
+
+function HeroSection() {
   return (
-    <Container fluid className="hero-section">
-      <Row>
-        <Col md={6}>
-          <h1>Get Expert Legal Advice</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            sit amet nulla auctor, vestibulum magna sed, convallis ex.
-          </p>
-          <Button variant="primary" size="lg">
-            Get Started
-          </Button>
-        </Col>
-        <Col md={6}>
-          <img src="hero-image.jpg" alt="Hero Image" />
-        </Col>
-      </Row>
-    </Container>
+    <Carousel>
+      {data?.map((item, index) => {
+        return (
+          <Carousel.Item key={index} interval={2000}>
+            {/* <img
+              className="d-block w-100"
+              src={item?.img}
+              alt={`Slide ${index}`}
+            /> */}
+            <Carousel.Caption>
+              <Card style={{ width: '100%', height: '200px' }}>
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Card.Link href="#">Card Link</Card.Link>
+                  <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+              </Card>
+            </Carousel.Caption>
+          </Carousel.Item>
+        );
+      })}
+    </Carousel>
   );
-};
+}
 
 export default HeroSection;
--- components/Navbar.js
+++ components/Navbar.js
@@ -5,11 +5,11 @@ import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 const NavigationBar = () => {
   return (
     <Navbar bg="light" expand="lg">
-      <Container>
+      <Container className='container-wrapper'>
         <Navbar.Brand href="#home">Legal Consultancy App</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+        <Navbar.Collapse id="basic-navbar-nav" className='nav-bar-options'>
+          <Nav className="mr-auto nav-wrapper">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/lawyers">Lawyers</Nav.Link>
             <Nav.Link href="/user/dashboard">Dashboard</Nav.Link>
